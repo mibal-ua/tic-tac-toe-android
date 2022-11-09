7382e1a0e27b6bc4c6e92a2a9557f1c87183e11b
mibal_ua <9mohapx9@gmail.com>
2022-11-09 17:35:59 +0100

    Create new activity for game table

---
+++ app/src/main/java/com/mibalstudio/tictactoe/GameActivity.java
@@ -0,0 +1,51 @@
+package com.mibalstudio.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.mibalstudio.tictactoe.component.DataPrinter;
+import com.mibalstudio.tictactoe.component.GameOverHandler;
+import com.mibalstudio.tictactoe.component.UserInputReader;
+import com.mibalstudio.tictactoe.model.game.Cell;
+import com.mibalstudio.tictactoe.model.game.GameTable;
+
+public class GameActivity extends AppCompatActivity implements DataPrinter, UserInputReader, GameOverHandler {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+    @Override
+    public void printInstructions() {
+
+    }
+
+    @Override
+    public void printInfoMessage(String message) {
+
+    }
+
+    @Override
+    public void printErrorMessage(String message) {
+
+    }
+
+    @Override
+    public void printGameTable(GameTable gameTable) {
+
+    }
+
+    @Override
+    public void gameOver() {
+
+    }
+
+    @Override
+    public Cell getUserInput() {
+        return null;
+    }
+}
