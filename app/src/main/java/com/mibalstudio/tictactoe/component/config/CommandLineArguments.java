bd3bdbb7a7243f34e762d52ed82e1b69e8140955
mibal_ua <9mohapx9@gmail.com>
2023-03-06 14:46:18 +0100

    Modify CommandLineArguments, del unusable fields

--- app/src/main/java/com/mibalstudio/tictactoe/component/config/CommandLineArguments.java
+++ app/src/main/java/com/mibalstudio/tictactoe/component/config/CommandLineArguments.java
@@ -12,34 +12,18 @@ import java.io.Serializable;
  */
 public class CommandLineArguments implements Serializable {
 
-    private final PlayerType player1Type;
-
     private final PlayerType player2Type;
-
-    private final UserInterface userInterface;
 
     private final Level level;
 
-    private CommandLineArguments(final PlayerType player1Type,
-                                 final PlayerType player2Type,
-                                 final UserInterface userInterface,
-                                 final Level level) {
-        this.player1Type = player1Type;
+    public CommandLineArguments(final PlayerType player2Type,
+                                final Level level) {
         this.player2Type = player2Type;
-        this.userInterface = userInterface;
         this.level = level;
-    }
-
-    public PlayerType getPlayer1Type() {
-        return player1Type;
     }
 
     public PlayerType getPlayer2Type() {
         return player2Type;
-    }
-
-    public UserInterface getUserInterface() {
-        return userInterface;
     }
 
     public Level getLevel() {
