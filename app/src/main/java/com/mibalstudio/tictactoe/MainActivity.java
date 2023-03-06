0ca3ab373cc5bf60c3ee833f8725c895ab4c0b90
mibal_ua <9mohapx9@gmail.com>
2023-03-06 15:00:45 +0100

    Implement MainActivity args configurat n handling

--- app/src/main/java/com/mibalstudio/tictactoe/MainActivity.java
+++ app/src/main/java/com/mibalstudio/tictactoe/MainActivity.java
@@ -1,13 +1,30 @@
 package com.mibalstudio.tictactoe;
 
+import static com.mibalstudio.tictactoe.R.id.level1Radio;
+import static com.mibalstudio.tictactoe.R.id.level2Radio;
+import static com.mibalstudio.tictactoe.R.id.level3Radio;
+import static com.mibalstudio.tictactoe.model.config.Level.LEVEL1;
+import static com.mibalstudio.tictactoe.model.config.Level.LEVEL2;
+import static com.mibalstudio.tictactoe.model.config.Level.LEVEL3;
+import static com.mibalstudio.tictactoe.model.config.PlayerType.COMPUTER;
+import static com.mibalstudio.tictactoe.model.config.PlayerType.USER;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.mibalstudio.tictactoe.component.config.CommandLineArguments;
+import com.mibalstudio.tictactoe.model.config.Level;
+import com.mibalstudio.tictactoe.model.config.PlayerType;
 
-import com.mibalstudio.tictactoe.component.Game;
-import com.mibalstudio.tictactoe.component.GameFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String ARGUMENTS_KEY = "args";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +32,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void start(final String[] args) {
-        // onCLick() create arguments and send it to gameFactory
-        final GameFactory gameFactory = new GameFactory(args);
-        Game game = gameFactory.create();
-        game.play();
+    public void start(final View view) {
+        final Intent intent = new Intent(this,
+                GameActivity.class);
+        final CommandLineArguments arguments = configureAllArguments();
+        intent.putExtra(ARGUMENTS_KEY, arguments);
+        startActivity(intent);
     }
 
+    private CommandLineArguments configureAllArguments() {
+        PlayerType player2Type;
+        final Switch aSwitch = findViewById(R.id.userSwitch);
+        if (aSwitch.isChecked()) {
+            player2Type = USER;
+        } else {
+            player2Type = COMPUTER;
+        }
+        final RadioGroup radios = findViewById(R.id.radioGroup);
+        final int radioId = radios.getCheckedRadioButtonId();
+        Level level = null;
+        if (radioId == level1Radio) {
+            level = LEVEL1;
+        }
+        if (radioId == level2Radio) {
+            level = LEVEL2;
+        }
+        if (radioId == level3Radio) {
+            level = LEVEL3;
+        }
+        return new CommandLineArguments(player2Type, level);
+    }
 }
