5bffd92fd4bd46966e73e615725e97cfb7e381d3
mibal_ua <9mohapx9@gmail.com>
2023-03-11 13:29:57 +0100

    Implement init logic of GameActivity
    - Add args printing for tests
    - Use second thread for game logic

--- app/src/main/java/com/mibalstudio/tictactoe/GameActivity.java
+++ app/src/main/java/com/mibalstudio/tictactoe/GameActivity.java
@@ -1,23 +1,44 @@
 package com.mibalstudio.tictactoe;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.mibalstudio.tictactoe.MainActivity.ARGUMENTS_KEY;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Looper;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.mibalstudio.tictactoe.component.DataPrinter;
+import com.mibalstudio.tictactoe.component.Game;
+import com.mibalstudio.tictactoe.component.GameFactory;
 import com.mibalstudio.tictactoe.component.GameOverHandler;
 import com.mibalstudio.tictactoe.component.UserInputReader;
+import com.mibalstudio.tictactoe.component.config.CommandLineArguments;
 import com.mibalstudio.tictactoe.model.game.Cell;
 import com.mibalstudio.tictactoe.model.game.GameTable;
 
 public class GameActivity extends AppCompatActivity implements DataPrinter, UserInputReader, GameOverHandler {
 
+    private Thread gameThread;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        final Intent intent = getIntent();
+        final CommandLineArguments args = (CommandLineArguments) intent.getSerializableExtra(ARGUMENTS_KEY);
+        printInfoMessage(args.getLevel() + " " + args.getPlayer2Type()); // TODO delete
+
+        gameThread = new Thread(() -> {
+            Looper.prepare();
+            GameFactory gameFactory = new GameFactory(args, this);
+            Game game = gameFactory.create();
+
+            game.play();
+        });
+        gameThread.start();
     }
 
     @Override
@@ -46,7 +67,7 @@ public class GameActivity extends AppCompatActivity implements DataPrinter, User
 
     @Override
     public void gameOver() {
-
+        // TODO
     }
 
     @Override
