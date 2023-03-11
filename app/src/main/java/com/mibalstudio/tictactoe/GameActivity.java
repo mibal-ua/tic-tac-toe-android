d731f51431a9a36a415a21e61fff00e6ef4de9c7
mibal_ua <9mohapx9@gmail.com>
2023-03-11 16:15:47 +0100

    Implement printGameTable method

--- app/src/main/java/com/mibalstudio/tictactoe/GameActivity.java
+++ app/src/main/java/com/mibalstudio/tictactoe/GameActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mibalstudio.tictactoe.component.CellToNumberConverter;
 import com.mibalstudio.tictactoe.component.DataPrinter;
 import com.mibalstudio.tictactoe.component.Game;
 import com.mibalstudio.tictactoe.component.GameFactory;
@@ -17,8 +18,11 @@ import com.mibalstudio.tictactoe.component.UserInputReader;
 import com.mibalstudio.tictactoe.component.config.CommandLineArguments;
 import com.mibalstudio.tictactoe.model.game.Cell;
 import com.mibalstudio.tictactoe.model.game.GameTable;
+import com.mibalstudio.tictactoe.model.game.Sign;
 
 public class GameActivity extends AppCompatActivity implements DataPrinter, UserInputReader, GameOverHandler {
+
+    public static final int COlS_N_ROWS = 3;
 
     private Thread gameThread;
 
@@ -60,9 +64,34 @@ public class GameActivity extends AppCompatActivity implements DataPrinter, User
         toast.show();
     }
 
-    @Override
-    public void printGameTable(GameTable gameTable) {
+    /*
+        cell id contract: cell${numberOfCell}
 
+            1 | 2 | 3
+            –––––––––
+            4 | 5 | 6
+            –––––––––
+            7 | 8 | 9
+
+        example: `android:id="@+id/cell4"`
+     */
+    @Override
+    public void printGameTable(final GameTable gameTable) {
+        runOnUiThread(() -> {
+            for (int i = 0; i < COlS_N_ROWS; i++) {
+                for (int j = 0; j < COlS_N_ROWS; j++) {
+                    final Cell cell = new Cell(i, j);
+                    final Sign sign = gameTable.getSign(cell);
+
+                    char cellId = new CellToNumberConverter().toNumber(cell);
+                    final ImageView imageView = findViewById(getResources().getIdentifier(
+                            "cell" + cellId, "id",
+                            this.getPackageName()));
+
+                    imageView.setImageResource(sign.getResourceId());
+                }
+            }
+        });
     }
 
     @Override
